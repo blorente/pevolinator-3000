f4b1424e5834bb2bb8dbd1f358f7624bcad8edf6
pegartillo95 <pegarc03@ucm.es>
2017-03-01 09:23:02 +0100

    Deleted firstFunction and moved to problem

--- src/model/solvers/problems/Problem.java
+++ src/model/solvers/problems/Problem.java
@@ -2,12 +2,21 @@ package model.solvers.problems;
 
 import model.solvers.fitness.Fitness;
 
-public abstract class Problem {
+public class Problem {
 	protected int populationSize;
 	protected int numberGenerations;
 	protected Fitness fitness;
 	protected double intervalMin;
 	protected double intervalMax;
+
+
+	public Problem(int populationSize, int numberGenerations, Fitness fitness, double intervalMin, double intervalMax){
+		this.populationSize = populationSize;
+		this.numberGenerations = numberGenerations;
+		this.fitness = fitness;
+		this.intervalMax = intervalMax;
+		this.intervalMin = intervalMin;
+	}
 	
 	public int getPopulationSize() {
 		return populationSize;
