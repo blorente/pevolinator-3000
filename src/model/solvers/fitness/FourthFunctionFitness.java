95a929c2a96abdf3ba6603d93249f5b6efab2151
Borja Lorente <blorente@ucm.es>
2017-03-17 19:52:39 +0100

    Fuck all!

--- src/model/solvers/fitness/FourthFunctionFitness.java
+++ src/model/solvers/fitness/FourthFunctionFitness.java
@@ -17,7 +17,13 @@ public class FourthFunctionFitness extends Fitness {
 			doubleGenes[i] = xn.get(i).doubleValue();
 		}
 		
-		double fTot = sumAll(new int[]{1, numGenes}, (a, i) -> sin(a) * pow(sin(((i + 1)*pow(a, 2))/PI()), 20), doubleGenes);
+		double fTot = 0.0;
+		for (int i = 0; i < doubleGenes.length; i++) {
+			double a = doubleGenes[i];
+			fTot += sin(a) * pow(sin(((i + 1)*pow(a, 2))/PI()), 20);
+		}
+		
+		//double fTot = sumAll(new int[]{1, numGenes}, (a, i) -> sin(a) * pow(sin(((i + 1)*pow(a, 2))/PI()), 20), doubleGenes);
 		return -fTot;
 	}
 
