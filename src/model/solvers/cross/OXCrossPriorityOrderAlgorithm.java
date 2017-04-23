4610749e9d9a82b1c54f97ff3209a84767d259d6
Borja Lorente <blorente@ucm.es>
2017-04-23 20:39:44 +0200

    Fix OXPrioOrder

--- src/model/solvers/cross/OXCrossPriorityOrderAlgorithm.java
+++ src/model/solvers/cross/OXCrossPriorityOrderAlgorithm.java
@@ -24,7 +24,7 @@ public class OXCrossPriorityOrderAlgorithm extends CrossAlgorithm {
 		int genomeSize = ind.getGenome().totalSize();
 		int numPoints = (int) (Math.random() * (ind.getGenome().totalSize() - 1));
 		
-		List<Integer> nums = this.getCrossPointsList(genomeSize, numPoints);
+		List<Integer> nums = this.getCrossPointsList(genomeSize + 1, numPoints);
 		
 		performOXPriorityOrderCross(ind, selected, nums);		
 	
@@ -43,25 +43,24 @@ public class OXCrossPriorityOrderAlgorithm extends CrossAlgorithm {
 		Gene firstChild[] = copyFromDiscreteInArrayPerPoint(ind.getGenome(), indPositions, genomeSize);
 		Gene secondChild[] = copyFromDiscreteInArrayPerPoint(selected.getGenome(), selectedPositions, genomeSize);
 		
-		fixGenomePrioOrder(firstChild, selected.getGenome(), selectedPositions, genomeSize);
-		fixGenomePrioOrder(secondChild, ind.getGenome(), indPositions, genomeSize);
+		fixGenomePrioOrder(firstChild, ind.getGenome(), selectedPositions, genomeSize);
+		fixGenomePrioOrder(secondChild, selected.getGenome(), indPositions, genomeSize);
 		
 		ind.getGenome().copyFromDiscrete(new Genome(arrayToList(firstChild)), 0, firstChild.length);
 		selected.getGenome().copyFromDiscrete(new Genome(arrayToList(secondChild)), 0, secondChild.length);
 		
 	}
 	
-	private void fixGenomePrioOrder(Gene[] firstChild, Genome genome, List<Integer> selectedPositions, int genomeSize) {
+	private void fixGenomePrioOrder(Gene[] child, Genome genome, List<Integer> positions, int genomeSize) {
 		int i = 0;
 		int j = 0;
 		while(i < genomeSize){
-			if(firstChild[i] == null){
-				firstChild[i] = genome.getGene(selectedPositions.get(j));
+			if(child[i] == null){
+				child[i] = genome.getGene(positions.get(j));
 				j++;
 			}
 			i++;
-		}
-		
+		}		
 	}
 
 	protected Gene[] copyFromDiscreteInArrayPerPoint(Genome genome, List<Integer> points, int genomeSize) {
